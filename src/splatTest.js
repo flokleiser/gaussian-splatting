85fd5cb8d8051aac82f785c7e55c5012be9a1cbc
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-03-27 19:19:33 +0100

    added cat test

--- src/splatTest.js
+++ src/splatTest.js
@@ -1,5 +1,4 @@
 import { WebGLRenderer, PerspectiveCamera, Scene } from 'three';
-// orbit controls
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import { LumaSplatsThree } from '@lumaai/luma-web';
@@ -10,11 +9,11 @@ renderer.domElement.style.position = 'absolute';
 renderer.domElement.style.width = '100%';
 renderer.domElement.style.height = '100%';
 
-// add canvas to DOM
 document.body.appendChild(renderer.domElement);
 
+// let camera = new PerspectiveCamera(75, 1, 0.1, 1000);
 let camera = new PerspectiveCamera(75, 1, 0.1, 1000);
-camera.position.z = 2;
+camera.position.z = -2;
 
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -22,9 +21,7 @@ controls.enableDamping = true;
 let scene = new Scene();
 
 let splat = new LumaSplatsThree({
-	source: 'https://lumalabs.ai/capture/d80d4876-cf71-4b8a-8b5b-49ffac44cd4a'
-
-	// add own splat
+	source: "https://lumalabs.ai/embed/dda54514-b5b8-4675-8d4c-a2590acbe399"
 });
 scene.add(splat);
 
